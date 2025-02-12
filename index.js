0d2c084129b4ac41c452aace8bff59e501c1c234
JennieGalmin <Jennie.Galmin@gmail.com>
2025-02-12 18:58:09 +0100

    delete knapp

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ const list = document.querySelector(".list");
 
 
 const listText = () => {
-    return list.insertAdjacentHTML("beforeend", `<li>${input.value}</li>` )
+    list.insertAdjacentHTML("beforeend", `<li>${input.value}     <button class="deleteBtn">x</button></li>`); 
+    input.value = "";
 }
 
+
 button.addEventListener("click", listText)
+
+list.addEventListener("click", (event) => {
+    if(event.target.classList.contains("deleteBtn"))
+        event.target.parentElement.remove()
+    
+})
